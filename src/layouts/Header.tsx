3b55a52b989700357e3704e24c897b705d55cc55
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-11-08 18:36:31 +0400

    ui: added header html

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="h-[6.25rem] flex items-center border-b-[1px] border-gray">
+        <div className="px-sm">
+            <h1 className="text-[4rem] text-[#F93B1D] font-jomhuria ">Novora</h1>
+        </div>
+    </header>
+  )
+}

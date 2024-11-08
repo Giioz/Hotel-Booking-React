38da83dd3cacb5c9fed424eb9cf6388d076cceac
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-11-08 18:35:57 +0400

    wip: tested api req calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Header } from './layouts/Header'
+import { getAllRoom } from './services/ApiService'
 
 function App() {
+  const [rooms, setRooms] = useState([]);
 
-  return <div>hotel booking</div>
+  useEffect(() => {
+   const getRooms = async () => {
+    try {
+      const res = await getAllRoom();
+      setRooms(res);
+    } catch(err){
+      return err;
+    }
+   }
+   
+   getRooms()
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <p onClick={() => {
+        console.log(rooms);
+        
+      }}>asd</p>
+    </>
+  )
 }
 
 export default App
